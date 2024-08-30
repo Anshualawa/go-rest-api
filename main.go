8852d1b619193431c3c5882eabdc647dcbfa044f
Pappu Alawa <alawapappu@gmail.com>
2024-08-30 23:34:33 +0530

    intial setup

--- main.go
+++ main.go
@@ -1,12 +1,30 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
 
+	utly "github.com/Anshualawa/go-rest-api/util"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
-	fmt.Println("Rest API")
 	e := echo.New()
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Welcome to Echo with Middlewre!")
+	})
+
+	e.POST("/user", func(c echo.Context) error {
+		u := new(utly.User)
+		if err := c.Bind(u); err != nil {
+			return err
+		}
+		return c.JSON(http.StatusOK, u)
+	})
+
+	e.Logger.Fatal(e.Start(":8080"))
 }
---
+++ util/Utiliy.go
@@ -0,0 +1,6 @@
+package utly
+
+type User struct {
+	Name  string `json:"name"`
+	Email string `json:"email"`
+}
